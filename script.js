7214dda0238430042490a66bc6aac5de6899eee0
huba-cat <huba0561@gmail.com>
2025-03-15 23:53:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menuButton").addEventListener("click", function () {
+    let menu = document.getElementById("smallMenu");
+    menu.style.display = (menu.style.display === "none") ? "block" : "none";
+});
